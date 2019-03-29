2d4b5a0e42562b62ad35b533d18b212dca02b2d2
Av8trix <47938305+Av8trix@users.noreply.github.com>
2019-03-29 09:26:57 -0700

    Delete azure_configs.py

--- azure_configs.py
+++
@@ -1,7 +0,0 @@
-import azure.storage.file.fileservice
-
-fs = azure.storage.file.fileservice.FileService(
-    account_name='sensitivedata', 
-    sas_token=<REDACTED>)
-
-fs.get_file_to_path('secrets', None, 'config.txt', 'config')
